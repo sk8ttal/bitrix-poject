073b4a10e1c859063db7b8b38fb3928eeaf2ab32
A.Fomishkin <fomishkin@bitrix.ru>
2022-03-09 18:07:08 +0200

    selenium package references added

--- ATlearning/ATframework3demo/Program.cs
+++ ATlearning/ATframework3demo/Program.cs
@@ -2,11 +2,9 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,12 +13,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
