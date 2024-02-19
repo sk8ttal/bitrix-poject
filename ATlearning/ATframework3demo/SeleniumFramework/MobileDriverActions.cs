2166fce67c3850e1584aafba151af1a458697c41
sernote <s.notevskiy@gmail.com>
2024-02-19 14:52:24 +0300

    правки мобильного драйвера для работы с новым аппиумом

--- ATlearning/ATframework3demo/SeleniumFramework/MobileDriverActions.cs
+++ ATlearning/ATframework3demo/SeleniumFramework/MobileDriverActions.cs
@@ -14,15 +14,16 @@ namespace atFrameWork2.SeleniumFramework
         public static AppiumDriver GetNewMobileDriver()
         {
             var appiumOptions = new AppiumOptions();
-            appiumOptions.AddAdditionalOption(MobileCapabilityType.PlatformName, "Android");
-            appiumOptions.AddAdditionalOption(MobileCapabilityType.Udid, "your_device_udid"); // Уникальный идентификатор устройства
-            appiumOptions.AddAdditionalOption(MobileCapabilityType.AutomationName, "UiAutomator2"); // Драйвер для автоматизации
-            appiumOptions.AddAdditionalOption(MobileCapabilityType.App, "/path/to/your/bitrix24.apk"); // Путь к приложению
-            appiumOptions.AddAdditionalOption(MobileCapabilityType.NoReset, true); // Не сбрасывать состояние приложения
-            appiumOptions.AddAdditionalOption(MobileCapabilityType.FullReset, false); // Выполнить полный сброс состояния
-            appiumOptions.AddAdditionalOption(MobileCapabilityType.NewCommandTimeout, 60); // Таймаут для новой команды
+            appiumOptions.PlatformName = "Android";
+            appiumOptions.AutomationName = "UiAutomator2"; // Драйвер для автоматизации
+            appiumOptions.App = "/path/to/your/bitrix24_stable.apk"; // Путь к приложению
+            appiumOptions.AddAdditionalAppiumOption(MobileCapabilityType.Udid, "emulator-5554"); // Уникальный идентификатор устройства
+            appiumOptions.AddAdditionalAppiumOption(MobileCapabilityType.NoReset, false); // Не сбрасывать состояние приложения
+            appiumOptions.AddAdditionalAppiumOption(MobileCapabilityType.FullReset, false); // Выполнить полный сброс состояния
+            appiumOptions.AddAdditionalAppiumOption(MobileCapabilityType.NewCommandTimeout, 60); // Таймаут для новой команды
+            appiumOptions.AddAdditionalAppiumOption(AndroidMobileCapabilityType.AutoGrantPermissions, true); // Автоматом подтверждает уведомления
             var appiumHost = "http://127.0.0.1:4723";
-            AppiumDriver driver = new AndroidDriver(new Uri($"{appiumHost}/wd/hub"), appiumOptions);
+            AppiumDriver driver = new AndroidDriver(new Uri($"{appiumHost}/"), appiumOptions);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             return driver;
         }
